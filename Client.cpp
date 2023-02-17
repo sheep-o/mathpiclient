b91eed8f2a5d45d4f761fcc55e892d46abaeac88
rami <rami@ramis-MacBook-Air.local>
2023-02-16 19:43:31 -0500

    x

--- Client.cpp
+++ Client.cpp
@@ -1,16 +1,20 @@
 #include "Client.h"
 #include <cassert>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 Client::Client(std::string &url) 
 {
 	std::cout << "Attempting to connect to WS server '" << url << "'\n";
 	ws = easywsclient::WebSocket::from_url(url);
 	assert(ws);
+
+	cap = std::make_unique<cv::VideoCapture>(2); 
 }
 
 Client::~Client() 
 {
+	cap.release();
 	delete ws;
 }
 
@@ -25,6 +29,14 @@ void Client::loop()
 void Client::run() 
 {
 	ws->poll();
-	std::cout << "Hi\n";
-	ws->send("andew");
+	
+	*cap.get() >> img;
+	if (img.empty()) return;
+
+	std::vector<uchar> char_buf;
+	cv::imencode(".jpg", img, char_buf);
+	std::string str_buf(char_buf.begin(), char_buf.end());
+
+	ws->send(str_buf);
+	cv::waitKey(1000);
 }
--- Client.h
+++ Client.h
@@ -1,6 +1,7 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <opencv2/opencv.hpp>
 #include "easywsclient.hpp"
 
 class Client 
@@ -13,6 +14,8 @@ public:
 	void run();
 private:
 	easywsclient::WebSocket::pointer ws; 
+	cv::Mat img;
+	std::unique_ptr<cv::VideoCapture> cap;
 };
 
 #endif
